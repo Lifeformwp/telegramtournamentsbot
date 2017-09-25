a4bdecbc0fde215023c36ec41251f50ed5489bbf
lifeformwp <vielon.indie@yandex.ru>
2017-09-25 22:50:39 +0300

    tournamentbottelegram-0.5-deleting-commands

--- vendor/longman/telegram-bot/src/Commands/SystemCommands/StartCommand.php
+++ vendor/longman/telegram-bot/src/Commands/SystemCommands/StartCommand.php
@@ -7,13 +7,14 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Longman\TelegramBot\Commands\SystemCommands;
+
 use Longman\TelegramBot\Commands\SystemCommand;
 use Longman\TelegramBot\Request;
+
 /**
  * Start command
- *
- * Gets executed when a user first starts using the bot.
  */
 class StartCommand extends SystemCommand
 {
@@ -21,37 +22,34 @@ class StartCommand extends SystemCommand
      * @var string
      */
     protected $name = 'start';
+
     /**
      * @var string
      */
     protected $description = 'Start command';
+
     /**
      * @var string
      */
     protected $usage = '/start';
+
     /**
      * @var string
      */
-    protected $version = '1.1.0';
-    /**
-     * @var bool
-     */
-    protected $private_only = true;
+    protected $version = '1.0.0';
+
     /**
      * Command execute method
      *
-     * @return \Longman\TelegramBot\Entities\ServerResponse
+     * @return mixed
      * @throws \Longman\TelegramBot\Exception\TelegramException
      */
     public function execute()
     {
-        $message = $this->getMessage();
-        $chat_id = $message->getChat()->getId();
-        $text    = 'Hi there!' . PHP_EOL . 'Type /help to see all commands!';
-        $data = [
-            'chat_id' => $chat_id,
-            'text'    => $text,
-        ];
-        return Request::sendMessage($data);
+        //$message = $this->getMessage();
+        //$chat_id = $message->getChat()->getId();
+        //$user_id = $message->getFrom()->getId();
+
+        return parent::execute();
     }
 }
