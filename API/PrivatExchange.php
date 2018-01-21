cc1fbd6c12259a87b9bcc72b165f5a2ef4d4a115
Serhii Kondratiuk <vielon.indie@yandex.ru>
2018-01-21 22:03:22 +0200

    Privat exchange

---
+++ API/PrivatExchange.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace API;
+
+
+class PrivatExchange
+{
+    const JSON_PRIVAT = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+
+    public function getExchange()
+    {
+        $exchangeData = json_decode(file_get_contents(static::JSON_PRIVAT));
+        $exchangeResult = "Курс валют:";
+
+        foreach ($exchangeData as $currency) {
+            $exchangeResult .= "\n 1 " . $currency['ccy'] . " стоит " . $currency['buy'] . " " . $currency['base_ccy'];
+        }
+
+        return $exchangeResult;
+    }
+}
--- commands/PrivatCommand.php
+++ commands/PrivatCommand.php
@@ -2,25 +2,28 @@
 
 namespace Longman\TelegramBot\Commands\UserCommands;
 
+use API\PrivatExchange;
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Request;
 
-class TestCommand extends UserCommand
+class PrivatCommand extends UserCommand
 {
-    protected $name = 'test';                      // Your command's name
-    protected $description = 'A command for test'; // Your command description
-    protected $usage = '/test';                    // Usage of your command
+    protected $name = 'privat';                      // Your command's name
+    protected $description = 'A command for exchange of privatbank'; // Your command description
+    protected $usage = '/privat';                    // Usage of your command
     protected $version = '1.0.0';                  // Version of your command
 
     public function execute()
     {
+        $privatAPI = new PrivatExchange();
+
         $message = $this->getMessage();            // Get Message object
 
         $chat_id = $message->getChat()->getId();   // Get the current Chat ID
 
         $data = [                                  // Set up the new message data
             'chat_id' => $chat_id,                 // Set Chat ID to send the message to
-            'text'    => 'This is just a Test...', // Set message to send
+            'text'    => $privatAPI->getExchange(), // Set message to send
         ];
 
         return Request::sendMessage($data);        // Send message!
